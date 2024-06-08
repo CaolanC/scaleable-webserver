918b91ea10de145af29aeaf599ed24f93c8fb631
CaolanC <caolancochrane@gmail.com>
2024-06-09 00:45:44 +0100

    feat(RestAPI): skeleton docker development environment createdOC

---
+++ dotnet/RabbitSender/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using System.Text;
+
+//ConnectionFactory factory = new();
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest",
+};
+
+factory.ClientProvidedName = "Rabbit Sender Application";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
